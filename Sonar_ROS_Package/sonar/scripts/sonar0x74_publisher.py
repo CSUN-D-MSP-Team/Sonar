d7e94f6b651387ec58c96e371ef3c42eb9f50cf6
Michael Granberry <43522518+michaelgranberryii@users.noreply.github.com>
2022-10-23 13:53:01 -0700

    Sonar package for ROS

---
+++ Sonar_ROS_Package/sonar/scripts/sonar0x74_publisher.py
@@ -0,0 +1,31 @@
+#----------------------------------------
+#Engineer: Michael Granberry
+#Project: ARCS Smart Pallet
+#Device: Ultrasonic Sensor
+#Model: I2C MaxSonar EZ Series - MB1212
+#Program: ROS Publisher Node for Sonar-0x74
+#Last Modified Date: Oct 23, 2022
+#----------------------------------------
+
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32
+from sonar import Sonar
+
+def sonar_talker():
+	pub = rospy.Publisher('sonar0x74_range_topic', Int32, queue_size=10) # publisher object
+	rospy.init_node('sonar0x74_publisher_node', anonymous=True) # initialize publisher node
+	rate = rospy.Rate(10) # ros rate
+	rospy.loginfo("Ros sonar node now publishing.")
+	s = Sonar(0x74)
+	while not rospy.is_shutdown():
+		rangeValue = s.read_range()
+		rospy.loginfo(rangeValue)
+		pub.publish(rangeValue)
+		rate.sleep()
+
+if __name__ == "__main__":
+	try:
+		sonar_talker()
+	except rospy.ROSInterruptException:
+		pass
